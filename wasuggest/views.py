3b79267f8fcd221b4aead81866d5a1e480778091
CoinSearchr <93884776+CoinSearchr@users.noreply.github.com>
2022-06-06 16:53:23 +0000

    Implement memoization of API calls
    - API call memoization (close #2)
    - config for WA API timeout (close #6)

--- wasuggest/views.py
+++ wasuggest/views.py
@@ -11,16 +11,76 @@ import pandas as pd
 import logging
 import random
 import urllib.parse
+#import dataclass
+from typing import List
 
-logger = logging.getLogger(__name__)
+logger = app.logger #logging.getLogger(__name__)
 
 site_config = db.config['site']
 
-# TODO add memoization for db.config['wolframalpha']['cache_timeout_sec']
+class cache_obj:
+	def __init__(self, first_result: str):
+		self.results_history: List[str] = [first_result]
+		self.last_new_result_time: float = time.time()
+		self.ever_had_varying_results: bool = False
+
+	def add_result(self, result: str):
+		""" Add a result, ensuring that the list doesn't get too long. """
+		logger.info(f"add_result(), {result in self.results_history}")
+		if result not in self.results_history:
+			self.ever_had_varying_results = True
+
+		self.results_history.append(result)
+		if len(self.results_history) > 20:
+			self.results_history.pop()
+
+		self.last_new_result_time = time.time()
+
+class MyMemoize:
+	"""
+	An advanced function memoization tool to cache API calls to WA to limit API call usage and increase speed.
+	Designed to play nice with random number generation requests, "what time is it", etc.from WolframAlpha.
+	"""
+	
+	def __init__(self, f):
+		self.f = f
+		self.memo = {}
+
+	def __call__(self, search_term):
+		if not search_term in self.memo:
+			# never been called before, not in cache
+			result = self.f(search_term)
+			self.memo[search_term] = cache_obj(result)
+			logger.info(f"New cache for search_term '{search_term}'.")
+			return result
+
+		elif len(self.memo[search_term].results_history) <= 2:
+			# not enough calls yet
+			result = self.f(search_term)
+			self.memo[search_term].add_result(result)
+			logger.info(f"Not enough calls for '{search_term}' to trust cache.")
+			return result
+
+		elif self.memo[search_term].ever_had_varying_results:
+			# had enough calls, but the results differ, and we know they differ
+			logger.info(f"Calls for search_term '{search_term}' have differed, so doing new call.")
+			return self.f(search_term)
+
+		elif (time.time() - self.memo[search_term].last_new_result_time) > (2 ** len(self.memo[search_term].results_history)):
+			# it's been a long time since we checked for a new result; check for a result to ensure it hasn't changed
+			logger.info(f"It's been a while for search_term '{search_term}' so checking there's no new result.")
+			result = self.f(search_term)
+			self.memo[search_term].add_result(result)
+			return result
+
+		logger.info(f"Using cached result for '{search_term}'.")
+		return self.memo[search_term].results_history[-1]
+
+@MyMemoize
 def wa_simple_lookup(search_term: str) -> str:
 	api_key = random.choice(db.config['wolframalpha']['api_keys_list'])
 
-	url = f'https://api.wolframalpha.com/v1/result?appid={api_key}&i={urllib.parse.quote_plus(search_term)}'
+	url = f'https://api.wolframalpha.com/v1/result?appid={api_key}&i={urllib.parse.quote_plus(search_term)}&timeout={db.config["wolframalpha"]["timeout_sec"]}'
 	logger.info(f"Making request to: {url}")
 
 	try:
@@ -54,7 +114,7 @@ def index():
 
 
 @app.route('/search', methods=['GET']) # args: q=search_term
-@cache.cached(timeout=30, query_string=True)
+@cache.cached(timeout=600, query_string=True)
 def search():
 	
 	arg_search_term_orig = request.args.get('q', '') # required for 'sanity check' by browser in suggestion mode
@@ -70,7 +130,7 @@ def search():
 
 
 @app.route('/suggest', methods=['GET']) # args: q=search_term
-@cache.cached(timeout=30, query_string=True)
+@cache.cached(timeout=1, query_string=True)
 def suggest():
 	"""
 	Provide JSON search suggestions.
--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,8 @@
 from wasuggest import app
 
 if __name__ == "__main__":
-    app.run()
+	if app.debug:
+		app.run(host='0.0.0.0')
+	else:
+		app.run()
 
